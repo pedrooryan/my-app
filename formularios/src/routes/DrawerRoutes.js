a26d4f7d1e45cb39dad1181150d48c9c4cd866aa
pedrooryan <pedro.ryan@iesb.edu.br>
2023-11-06 15:16:35 -0300

    Atividade Async storage

--- formularios/src/routes/DrawerRoutes.js
+++ formularios/src/routes/DrawerRoutes.js
@@ -7,18 +7,23 @@ import ListaCarros from '../screens/ListaCarros/ListaCarros'
 import ListaCarrosAsyncStorage from '../screens/ListaCarrosAsyncStorage/ListaCarros'
 import StackPessoas from '../screens/Pessoas/StackPessoas'
 import StackPessoasAsyncStorage from '../screens/PessoasAsyncStorage/StackPessoasAsyncStorage'
+import StackPessoasFormularioAltoNivel from '../screens/PessoasFormularioAltoNivel/StackPessoasFormularioAltoNivel'
+import Cadastro from '../screens/Cadastro/Cadastro'
 
 const Drawer = createDrawerNavigator()
 
 export default function DrawerRoutes() {
     return (
-        <Drawer.Navigator initialRouteName='PessoasAsyncStorage'>
+        <Drawer.Navigator initialRouteName='Cadastro'>
 
             <Drawer.Screen name="Home" component={Home} />
             <Drawer.Screen name="Carros" component={ListaCarros} />
             <Drawer.Screen name="CarrosAsyncStorage" component={ListaCarrosAsyncStorage} />
             <Drawer.Screen name="Pessoas" component={StackPessoas} />
             <Drawer.Screen name="PessoasAsyncStorage" component={StackPessoasAsyncStorage} />
+            <Drawer.Screen name="FormularioAltoNivel" component={StackPessoasFormularioAltoNivel} />
+
+            <Drawer.Screen name="Cadastro" component={Cadastro} />
 
         </Drawer.Navigator>
 
